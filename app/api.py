0021f4609cc3c035c29010cfe690375b43dfdc0c
AlexanderPease <alexander@usv.com>
2014-11-14 14:55:07 -0500

    Add coverage engine from Thomas

--- app/api.py
+++ app/api.py
@@ -10,58 +10,58 @@ from db.entrydb import Entry
 ### api/addentry
 ########################
 class AddEntry(app.basic.BaseHandler):
-  def post(self):
-    logging.info("POST to /addentry")
-    session_id = self.get_argument('session_id','')
-    age = self.get_argument('age','')
-    gender = self.get_argument('gender','')
-    kids = self.get_argument('kids','')
-    income = self.get_argument('income','')
-    mortgage = self.get_argument('mortgage','')
-    height = self.get_argument('height','')
-    weight = self.get_argument('weight','')
-    coverage_amount = self.get_argument('coverage_amount','')
-    coverage_term = self.get_argument('coverage_term','')
-    monthly_quote = self.get_argument('monthly_quote','')
+    def post(self):
+        logging.info("POST to /addentry")
+        session_id = self.get_argument('session_id','')
+        age = self.get_argument('age','')
+        gender = self.get_argument('gender','')
+        kids = self.get_argument('kids','')
+        income = self.get_argument('income','')
+        mortgage = self.get_argument('mortgage','')
+        height = self.get_argument('height','')
+        weight = self.get_argument('weight','')
+        coverage_amount = self.get_argument('coverage_amount','')
+        coverage_term = self.get_argument('coverage_term','')
+        monthly_quote = self.get_argument('monthly_quote','')
 
-    if session_id:
-      try:
-        entry = Entry.objects.get(session_id=session_id)
-        logging.info("Entry alread exists for session_id: %s" % session_id)
-      except:
-        entry = None
+        if session_id:
+            try:
+                entry = Entry.objects.get(session_id=session_id)
+                logging.info("Entry alread exists for session_id: %s" % session_id)
+            except:
+                entry = None
 
-      # If entry already exists, it's the same user clicking again. Ignore.
-      if not entry:
-        try:
-          entry = Entry(date=datetime.datetime.now(),
-            session_id=session_id,
-            age=age,
-            gender=gender,
-            kids=kids,
-            income=income,
-            mortgage=mortgage,
-            height=height,
-            weight=weight,
-            coverage_amount=coverage_amount,
-            coverage_term=coverage_term,
-            monthly_quote=monthly_quote
-          )
-          entry.save()
-          logging.info()
-        except:
-          logging.warning('Could not create entry:')
-          logging.warning('Age: %s' % age)
-          logging.warning('Gender: %s' % gender)
-          logging.warning('Kids: %s' % kids)
-          logging.warning('Income: %s' % income)
-          logging.warning('Mortgage: %s' % mortgage)
-          logging.warning('Height: %s' % height)
-          logging.warning('Weight: %s' % weight)
-          logging.warning('Coverage Amount: %s' % coverage_amount)
-          logging.warning('Coverage Term: %s' % coverage_term)
-          logging.warning('Monthyl Quote: %s' % monthly_quote)
-    return
+            # If entry already exists, it's the same user clicking again. Ignore.
+            if not entry:
+                try:
+                    entry = Entry(date=datetime.datetime.now(),
+                        session_id=session_id,
+                        age=age,
+                        gender=gender,
+                        kids=kids,
+                        income=income,
+                        mortgage=mortgage,
+                        height=height,
+                        weight=weight,
+                        coverage_amount=coverage_amount,
+                        coverage_term=coverage_term,
+                        monthly_quote=monthly_quote
+                    )
+                    entry.save()
+                    logging.info('Saved entry')
+                except:
+                    logging.warning('Could not create entry:')
+                    logging.warning('Age: %s' % age)
+                    logging.warning('Gender: %s' % gender)
+                    logging.warning('Kids: %s' % kids)
+                    logging.warning('Income: %s' % income)
+                    logging.warning('Mortgage: %s' % mortgage)
+                    logging.warning('Height: %s' % height)
+                    logging.warning('Weight: %s' % weight)
+                    logging.warning('Coverage Amount: %s' % coverage_amount)
+                    logging.warning('Coverage Term: %s' % coverage_term)
+                    logging.warning('Monthly Quote: %s' % monthly_quote)
+        return
 
 ########################
 ### ModalEntry
@@ -69,24 +69,26 @@ class AddEntry(app.basic.BaseHandler):
 ### api/modalentry
 ########################
 class ModalEntry(app.basic.BaseHandler):
-  def post(self):
-    logging.info("POST to /modalentry")
-    session_id = self.get_argument('session_id','')
-    name = self.get_argument('name','')
-    email = self.get_argument('email','')
+    def post(self):
+        logging.info("POST to /modalentry")
+        session_id = self.get_argument('session_id','')
+        name = self.get_argument('name','')
+        email = self.get_argument('email','')
 
-    try:
-      entry = Entry.objects.get(session_id=session_id)
-    except:
-      logging.info("Could not find Entry of session_id %s" % session_id)
-      logging.warning('Name: %s' % name)
-      logging.warning('Email: %s' % email)
-      return
+        try:
+            entry = Entry.objects.get(session_id=session_id)
+        except:
+            logging.info("Could not find Entry of session_id %s" % session_id)
+            logging.warning('Name: %s' % name)
+            logging.warning('Email: %s' % email)
+            return
 
-    if entry:
-      try:
-        entry.name = name
-      except:
-        logging.warning('Could not add name')
-      entry.email = email
-    return
+        if entry:
+            try:
+                entry.name = name
+                entry.email = email
+                entry.save()
+                logging.info("Saved name and email to entry of session_id: %s" % entry.session_id)
+            except:
+                logging.info("Could not add name or email to entry of session_id: %s" % entry.session_id)
+        return
